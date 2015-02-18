65d145860d1ea8e0a533250968e0b5490c4ba733
Kirk North <kirk.w.north@gmail.com>
2015-02-18 00:11:27 -0500

    WIP: building texture fields
    Due to a name change of the texture fields source code (Fortran 90), I
    subsequently had to reflect these changes in the build scripts.

--- clutter/texture/rebuild_texture.sh
+++ clutter/texture/rebuild_texture.sh
@@ -1,5 +1,5 @@
 # Remove and rebuild _texture_fields.so
 
-rm _texture_fields.so
-f2py -m texture_fields -h _texture_fields.pyf src/texture_fields.f90
-f2py --fcompiler=gfortran -c _texture_fields.pyf src/texture_fields.f90 
+rm -f _compute_texture.so
+f2py -m compute_texture -h _compute_texture.pyf src/compute_texture.f90
+f2py --fcompiler=gfortran -c _compute_texture.pyf src/compute_texture.f90 
--- clutter/texture/setup.py
+++ clutter/texture/setup.py
@@ -10,9 +10,9 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('texture', parent_package, top_path)
 
     # Add texture fields extension
-    source = ['_texture_fields.pyf', 'src/texture_fields.f90']
+    source = ['_compute_texture.pyf', 'src/compute_texture.f90']
     config.add_extension(
-        'texture_fields', sources=source, f2py_options=[])
+        'compute_texture', sources=source, f2py_options=[])
 
     return config
 
