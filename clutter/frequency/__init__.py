fb59f6ea022c582b2de8b9dc2c2b56f5a2dc4626
Kirk North <kirk.w.north@gmail.com>
2015-02-19 16:32:38 -0500

    NEW: clutter frequency maps
    Clutter frequency (probability) maps are obtained by computing the
    frequency each pixel in a radar scan is present (i.e. has a coherent
    signal) during non-precipitating events. See Rico-Ramirez and Cluckie
    (2008) for more information.
    
    Rico-Ramirez, M. A. and I. D. Cluckie, 2008: Classification of Ground
    Clutter and Anomolous Propagation Using Dual-Polarization Weather Radar.

---
+++ clutter/frequency/__init__.py
@@ -0,0 +1,4 @@
+"""
+"""
+
+from . import clutter_frequency
---
+++ clutter/frequency/clutter_frequency.py
@@ -0,0 +1,119 @@
+"""
+clutter.frequency.clutter_frequency
+===================================
+
+"""
+
+import os
+import re
+import numpy as np
+
+from datetime import datetime
+
+from pyart.io import read_sigmet, write_cfradial
+from pyart.config import get_field_name
+
+
+def map_date_range(start, stop, inpdir, stamp, date_str='[0-9]{12}',
+                   date_fmt='%y%m%d%H%M%S', min_ncp=0.3, num_sweeps=22,
+                   exclude_fields=None, refl_field=None, ncp_field=None,
+                   debug=False, verbose=False):
+    """
+    Compute the clutter frequency (probability) map within the specified date
+    range. The start and stop times should define a non-precipitating time
+    period where (most) echoes present must be, by definition, clutter.
+
+    Parameters
+    ----------
+
+    Optional Parameters
+    -------------------
+
+    Returns
+    -------
+    """
+
+    # Parse field names
+    if refl_field is None:
+        refl_field = get_field_name('reflectivity')
+    if ncp_field is None:
+        ncp_field = get_field_name('normalized_coherent_power')
+
+    # Get all files with stamp in directory
+    files = [os.path.join(inpdir, f) for f in sorted(os.listdir(inpdir))
+             if stamp in f]
+
+    if verbose:
+        print 'Total number of radar files found = %i' % len(files)
+
+    # Remove files outside date range
+    time_str = [re.search(date_str, f).group() for f in files]
+    times = [datetime.strptime(string, date_fmt) for string in time_str]
+    files = [
+        f for f, time in zip(files, times) if time >= start and time <= stop]
+
+    if verbose:
+        print 'Number of radar files after within date range = %i' % len(files)
+
+    # Loop over all files
+    clutter = []
+    for f in files:
+
+        if verbose:
+            print 'Processing file %s' % os.path.basename(f)
+
+        # Read radar data
+        radar = read_sigmet(
+            f, time_ordered='none', exclude_fields=exclude_fields)
+
+        # Make sure radar has proper number of sweeps
+        # This is a way to check that VCPs are consistent
+        if radar.nsweeps < num_sweeps:
+            continue
+
+        # Find pixels that have a coherent signal
+        ncp = radar.fields[ncp_field]['data']
+        ncp = np.ma.masked_where(ncp < min_ncp, ncp, copy=False)
+        clutter.append(np.logical_not(ncp.mask).astype(np.float64))
+
+    # Compute the probability a pixel (gate) is clutter
+    sample_size = len(clutter)
+    clutter_prob = np.sum(clutter, axis=0) / sample_size
+
+    return {
+        'clutter frequency map': clutter_prob,
+        'start time': start,
+        'end time': stop,
+        'last radar': radar
+        'sample size': sample_size
+    }
+
+
+def map_json_list(filename, inpdir=None, date_str='[0-9]{12}',
+                  date_fmt='%y%m%d%H%M%S', min_ncp=0.3, num_sweeps=22,
+                  exclude_fields=None, refl_field=None, ncp_field=None,
+                  ebug=False, verbose=False):
+    """
+    Compute the clutter frequency (probability) map from the files listed in a
+    JSON file. The listed files should define a non-precipitating time period
+    where (most) echoes present must be, by definition, clutter.
+
+    Parameters
+    ----------
+
+    Optional Parameters
+    -------------------
+
+    Returns
+    -------
+    """
+
+
+
+
+
+
+
+
+
+    return
