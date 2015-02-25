288c1edb0f88a3e3e2cd81ddd70599bd7928332b
Kirk North <kirk.w.north@gmail.com>
2015-02-24 23:50:06 -0500

    WIP: moment histogram from JSON
    Updating the histogram_from_json function in the moment module.

--- clutter/moment/moment_fields.py
+++ clutter/moment/moment_fields.py
@@ -15,7 +15,7 @@ def _pickle_histograms(histograms, filename, outdir=None):
 
 
 def histogram_from_json(
-        filename, field, inpdir=None, bins=10, limits=None, min_ncp=0.3,
+        filename, field, inpdir=None, bins=10, limits=None, min_ncp=0.5,
         vcp_sweeps=22, min_sweep=None, max_sweep=None, exclude_fields=None,
         fill_value=None, ncp_field=None):
     """
@@ -40,5 +40,37 @@ def histogram_from_json(
     if verbose:
         print 'Total number of radar files to process = %i' % len(files)
 
+    # Loop over all files
+    histogram = np.zeros(bins, dtype=np.float64)
+    for f in files:
 
-    return
+        # Read radar data
+        radar = read(f, exclude_fields=exclude_fields)
+
+        if radar.nsweeps != vcp_sweeps:
+            continue
+
+        if verbose:
+            print 'Processing file %s' % os.path.basename(f)
+
+        # Parse data and compute histogram
+        data = radar.fields[field]['data']
+        hist, edges = np.histogram(
+            data.compressed(), bins=bins, range=limits, normed=False,
+            weights=None, density=False)
+        histogram += hist
+
+    return {
+        'field': radar.fields[field]['long_name'],
+        'histogram': histogram,
+        'normalized histogram': histogram / histogram.max(),
+        'number of bins': bins,
+        'limits': limits,
+        'bin edges': edges,
+        'bin centers': edges[:-1] + np.diff(edges) / 2.0,
+        'radar files': [os.path.basename(f) for f in files],
+        'min sweep': min_sweep,
+        'max sweep': max_sweep,
+        'min normalized coherent power': min_ncp,
+        'sweeps in VCP': vcp_sweeps,
+        }
