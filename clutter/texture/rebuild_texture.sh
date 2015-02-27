5fd0367e6d990dfc3b438f8623b6eaf3be1513b6
Kirk North <kirk.w.north@gmail.com>
2015-02-27 17:13:07 -0500

    WIP: rebuild Fortran routines
    Fixing a minor typo in the Fortran rebuild routines...

--- clutter/texture/rebuild_texture.sh
+++ clutter/texture/rebuild_texture.sh
@@ -1,5 +1,5 @@
 # Remove and rebuild _texture_fields.so
 
-rm -f _compute_texture.so
+rm -f compute_texture.so
 f2py -m compute_texture -h _compute_texture.pyf src/compute_texture.f90
 f2py --fcompiler=gfortran -c _compute_texture.pyf src/compute_texture.f90 
