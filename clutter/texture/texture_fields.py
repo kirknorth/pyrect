17e2adfb7ccb8826cea38296345c215ab0b7dd9d
Kirk North <kirk.w.north@gmail.com>
2015-03-26 14:34:55 -0400

    WIP: update texture fields
    Texture fields and its associated processing now can use the work I've
    done on significant detection with the coherency fields. An update to
    the textures histogram figure is thus in order. Finally, minimum and
    maximum ranges can be specified when computing texture fields (e.g., to
    avoid ground clutter close to the radar). Some minor changes are likely
    to follow...

--- clutter/texture/texture_fields.py
+++ clutter/texture/texture_fields.py
@@ -25,10 +25,13 @@ def _pickle_histograms(histograms, filename, outdir=None):
     if outdir is not None:
         filename = os.path.join(outdir, filename)
 
-    # Create dictionary containing all texture fields organized by field name
-    data = {}
-    for histogram in histograms:
-        data[histogram['field']] = histogram
+    # Create dictionary from histogram list
+    if isinstance(histograms, list):
+        data = {histogram['field']: histogram for histogram in histograms}
+    elif isinstance(histograms, dict):
+        data = histograms
+    else:
+        raise ValueError('Unsupported histogram type')
 
     with open(filename, 'wb') as fid:
         pickle.dump(data, fid, protocol=pickle.HIGHEST_PROTOCOL)
@@ -36,8 +39,9 @@ def _pickle_histograms(histograms, filename, outdir=None):
     return
 
 
-def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
-                   min_ncp=0.5, min_sweep=None, max_sweep=None,
+def _compute_field(radar, field, gatefilter=None, ray_window=3, gate_window=3,
+                   min_sample=5, min_ncp=None, min_sweep=None, max_sweep=None,
+                   min_range=None, max_range=None, rays_wrap_around=False,
                    fill_value=None, ncp_field=None):
     """
     Compute the texture (standard deviation) within the 2-D window for the
@@ -61,7 +65,7 @@ def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
     if ncp_field is None:
         ncp_field = get_field_name('normalized_coherent_power')
 
-    # Parse radar field
+    # Parse radar data
     data = radar.fields[field]['data']
 
     # Mask sweeps outside of specified range
@@ -72,17 +76,30 @@ def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
         i = radar.sweep_end_ray_index['data'][max_sweep]
         data[i+1:,:] = np.ma.masked
 
+    # Mask radar range gates outside specified range
+    if min_range is not None:
+        i = np.abs(radar.range['data'] / 1000.0 - min_range).argmin()
+        data[:,:i+1] = np.ma.masked
+    if max_range is not None:
+        i = np.abs(radar.range['data'] / 1000.0 - max_range).argmin()
+        data[:,i+1:] = np.ma.masked
+
     # Mask incoherent echoes
     if min_ncp is not None:
         ncp = radar.fields[ncp_field]['data']
         data = np.ma.masked_where(ncp < min_ncp, data)
 
-    # Fill in masked values
-    data = np.ma.filled(data, fill_value).astype(np.float64)
+    # Mask excluded gates
+    if gatefilter is not None:
+        data = np.ma.masked_where(gatefilter.gate_excluded, data)
 
-    # Parse sweep parameters
-    # Need to add 1 to sweep index arrays in order to be consistent with
-    # Fortran array indexing
+    # Prepare data for ingest into Fortran wrapper
+    data = np.ma.filled(data, fill_value)
+    data = np.asfortranarray(data, dtype=np.float64)
+
+    # Parse sweep start/end indices
+    # Offset indices in order to be compatible with Fortran and avoid a
+    # segmentation fault
     sweep_start = radar.sweep_start_ray_index['data'] + 1
     sweep_end = radar.sweep_end_ray_index['data'] + 1
 
@@ -99,6 +116,7 @@ def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
 
     # Mask invalid values
     texture = np.ma.masked_equal(texture, fill_value, copy=False)
+    texture = np.ma.masked_invalid(texture, copy=False)
 
     texture = {
         'data': texture,
@@ -107,6 +125,8 @@ def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
         'long_name': '{} texture'.format(radar.fields[field]['long_name']),
         '_FillValue': texture.fill_value,
         'units': radar.fields[field]['units'],
+        'valid_min': 0.0,
+        'valid_max': texture.max(),
         'comment_1': ('Texture field is defined as the standard deviation '
                     'of a field within a prescribed window'),
         'comment_2': 'Window size was {} x {}'.format(gate_window, ray_window),
@@ -117,9 +137,10 @@ def _compute_field(radar, field, ray_window=3, gate_window=3, min_sample=None,
     return
 
 
-def add_textures(radar, fields=None, ray_window=3, gate_window=3,
-                 min_sample=None, min_ncp=0.5, min_sweep=None, max_sweep=None,
-                 fill_value=None, ncp_field=None):
+def add_textures(radar, fields=None, gatefilter=None, texture_window=(3, 3),
+                 texture_sample=5, min_ncp=None, min_sweep=None,
+                 max_sweep=None, min_range=None, max_range=None,
+                 rays_wrap_around=False, fill_value=None, ncp_field=None):
     """
     """
 
@@ -134,19 +155,26 @@ def add_textures(radar, fields=None, ray_window=3, gate_window=3,
     if fields is None:
         fields = radar.fields.keys()
 
+    # Parse texture window parameters
+    ray_window, gate_window = texture_window
+
     for field in fields:
         _compute_field(
-            radar, field, ray_window=ray_window, gate_window=gate_window,
-            min_sample=min_sample, min_ncp=min_ncp, min_sweep=min_sweep,
-            max_sweep=max_sweep, fill_value=fill_value, ncp_field=ncp_field)
+            radar, field, gatefilter=gatefilter, ray_window=ray_window,
+            gate_window=gate_window, min_sample=texture_sample,
+            min_ncp=min_ncp, min_sweep=min_sweep, max_sweep=max_sweep,
+            min_range=min_range, max_range=max_range,
+            rays_wrap_around=rays_wrap_around, fill_value=fill_value,
+            ncp_field=ncp_field)
 
     return
 
 
 def histogram_from_json(
-        filename, field, inpdir=None, ray_window=3, gate_window=3,
-        min_sample=None, bins=10, limits=None, min_ncp=0.5, vcp_sweeps=None,
-        vcp_rays=None, min_sweep=None, max_sweep=None, exclude_fields=None,
+        filename, field, inpdir=None, texture_window=(3, 3), min_sample=5,
+        num_bins=10, limits=None, min_ncp=0.5, vcp_sweeps=None,
+        vcp_rays=None, min_sweep=None, max_sweep=None, min_range=None,
+        max_range=None, rays_wrap_around=False, exclude_fields=None,
         fill_value=None, ncp_field=None, verbose=False):
     """
     """
@@ -170,8 +198,11 @@ def histogram_from_json(
     if verbose:
         print 'Total number of radar files to process = %i' % len(files)
 
+    # Parse texture window parameters
+    ray_window, gate_window = texture_window
+
     # Loop over all files
-    histogram = np.zeros(bins, dtype=np.float64)
+    counts = np.zeros(num_bins, dtype=np.float64)
     for f in files:
 
         # Read radar data
@@ -190,37 +221,92 @@ def histogram_from_json(
         _compute_field(
             radar, field, ray_window=ray_window, gate_window=gate_window,
             min_sample=min_sample, min_ncp=min_ncp, min_sweep=min_sweep,
-            max_sweep=max_sweep, fill_value=fill_value, ncp_field=ncp_field)
+            max_sweep=max_sweep, min_range=min_range, max_range=max_range,
+            rays_wrap_around=rays_wrap_around, fill_value=fill_value,
+            ncp_field=ncp_field)
 
         # Parse data and compute histogram
         data = radar.fields['{}_texture'.format(field)]['data']
         hist, bin_edges = np.histogram(
-            data.compressed(), bins=bins, range=limits, normed=False,
+            data.compressed(), bins=num_bins, range=limits, normed=False,
             weights=None, density=False)
-        histogram += hist
+        counts += hist
 
     # Compute bin centers
     bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2.0
 
     # Compute normalized histogram and probability density
-    histogram_norm = histogram / histogram.max()
-    pdf = histogram_norm / np.sum(histogram_norm * np.diff(bin_edges))
+    counts_norm = counts / counts.max()
+    pdf = counts_norm / np.sum(counts_norm * np.diff(bin_edges))
 
     return {
         'field': '{}_texture'.format(field),
-        'histogram': histogram,
-        'normalized histogram': histogram_norm,
+        'histogram counts': counts,
+        'normalized histogram': counts_norm,
         'probability density': pdf,
-        'number of bins': bins,
+        'number of bins': num_bins,
         'limits': limits,
         'bin edges': bin_edges,
         'bin centers': bin_centers,
         'radar files': [os.path.basename(f) for f in files],
         'min sweep': min_sweep,
         'max sweep': max_sweep,
+        'min range': min_range,
+        'max range': max_range,
         'min normalized coherent power': min_ncp,
         'sweeps in VCP': vcp_sweeps,
         'rays in VCP': vcp_rays,
         'ray window size': ray_window,
         'gate window size': gate_window,
         }
+
+def histograms_from_radar(
+        radar, hist_dict, gatefilter=None, texture_window=(3, 3),
+        texture_sample=5, min_ncp=None, min_sweep=None, max_sweep=None,
+        min_range=None, max_range=None, rays_wrap_around=False,
+        fill_value=None, ncp_field=None, verbose=False):
+    """
+    """
+
+    # Parse fill value
+    if fill_value is None:
+        fill_value = get_fillvalue()
+
+    # Parse field names
+    if ncp_field is None:
+        ncp_field = get_field_name('normalized_coherent_power')
+
+    # TODO: check input histogram dictionary for proper keys
+
+    # Parse texture window parameters
+    ray_window, gate_window = texture_window
+
+    # Loop over all fields and compute histogram counts
+    for field in hist_dict:
+
+        # Compute texture fields
+        _compute_field(
+            radar, field, gatefilter=gatefilter, ray_window=ray_window,
+            gate_window=gate_window, min_sample=texture_sample,
+            min_ncp=min_ncp, min_sweep=min_sweep, max_sweep=max_sweep,
+            min_range=min_range, max_range=max_range,
+            rays_wrap_around=rays_wrap_around, fill_value=fill_value,
+            ncp_field=ncp_field)
+
+        # Parse histogram parameters
+        bins = hist_dict[field]['number of bins']
+        limits = hist_dict[field]['limits']
+
+        # Parse data and compute histogram
+        data = radar.fields['{}_texture'.format(field)]['data']
+        counts, bin_edges = np.histogram(
+            data.compressed(), bins=bins, range=limits, normed=False,
+            weights=None, density=False)
+        hist_dict[field]['histogram counts'] += counts
+
+        # Compute bin centers and add to dictionary
+        bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2.0
+        hist_dict[field]['bin edges'] = bin_edges
+        hist_dict[field]['bin centers'] = bin_centers
+
+    return
